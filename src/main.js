c83dc5cd01637a8f21796d1426c757940619fc9f
Carlos <carlos.av.amorim@gmail.com>
2022-09-21 16:21:57 -0300

    Feat: Show users on screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log("testando...");
+const tokken =
+  "592116e2b217eea89dbd106c1736451b52d79a4b1d0e0fd8ea526c3ec0c2bd32";
+
+const baseURL = "https://gorest.co.in/public/v2";
+
+const getUsers = async () => {
+  const tbody = document.querySelector(".tbody");
+  const response = await fetch(`${baseURL}/users`).then((users) =>
+    users.json()
+  );
+
+  for (let i = 0; i < response.length; i++) {
+    let tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${response[i].id}</td>
+      <td>${response[i].name}</td>
+      <td>${response[i].email}</td>
+      <td class="action-button">
+        <button onclick="editUser(${response[i].id})">Editar</button>
+      </td>
+      <td class="action-button">
+        <button onclick="deleteUser(${response[i].id})">Excluir</button>
+      </td>     
+    `;
+    tbody.appendChild(tr);
+  }
+};
+
+getUsers();
